9f0d5185f54ad541839d9e87dd9e1a96f3a5c39e
FerLyNoX <danyakovenko123@gmail.com>
2023-05-23 12:13:27 +0200

    abc

---
+++ src/Strony/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (<div>
+        <a href='#home'>Home </a>
+        <a> - </a>
+        <a href='#News_Feed'>News Feed </a>
+        <a> - </a>
+        <a href='#messages'>Messages</a>
+    </div>);
+}
+export default Header;
