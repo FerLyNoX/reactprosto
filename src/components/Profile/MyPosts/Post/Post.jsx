5cfd87bb96d3d67a19002f963688b17c182c8a91
FerLyNoX <danyakovenko123@gmail.com>
2023-05-27 18:52:11 +0200

    push 2

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,17 @@
+import './Post.module.css';
+import s from './Post.module.css';
+
+const Post = (props) => {
+    return (
+        <div className={s.item}>
+            <img
+                src='https://w7.pngwing.com/pngs/862/646/png-transparent-beard-hipster-male-man-avatars-xmas-giveaway-icon-thumbnail.png'/>
+            {props.message}
+            <div>
+                <span> like </span>{props.likeCount}
+            </div>
+        </div>
+    )
+}
+
+export default Post;
